683211655939c4d5e4abede167765704439932f1
Edqe_ <34704796+Edqe14@users.noreply.github.com>
2023-01-14 00:35:14 +0700

    refactor: favicon

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -12,6 +12,7 @@ export default function Meta({ page = 'Home' }: Props) {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
 
       <meta name="description" content="BONFiRE is an online communication service for people!" />
+      <meta name="theme-color" content="#101921" />
     </Head>
   );
 }
