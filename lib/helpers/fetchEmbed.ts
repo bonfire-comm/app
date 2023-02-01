a550035b4cc63f8dcb213291443e90e49a6640b0
Edqe_ <34704796+Edqe14@users.noreply.github.com>
2023-02-01 12:48:50 +0700

    refactor: cache embed

--- lib/helpers/fetchEmbed.ts
+++ lib/helpers/fetchEmbed.ts
@@ -1,6 +1,11 @@
 import fetcher from '../api/fetcher';
 
+const cache = new Map<string, EmbedData>();
+
 export default async function fetchEmbed(url: string, cancelSignal?: AbortSignal) {
+  const cached = cache.get(url);
+  if (cached) return cached;
+
   const fetched = await fetcher.get<SemanticResponse<EmbedData>>(
     `/embed?url=${encodeURIComponent(url)}`,
     {
@@ -10,6 +15,12 @@ export default async function fetchEmbed(url: string, cancelSignal?: AbortSignal
       }
     }
   ).then((res) => res.data.payload).catch(() => null);
+
+  if (fetched) {
+    cache.set(url, fetched);
+
+    setTimeout(() => cache.delete(url), 60_000); // 1 minute
+  }
 
   return fetched;
 }
